bb65e8348a79234380ee5d75177aac00c9056e0f
Paciupa <65620103+Paciupa@users.noreply.github.com>
2024-12-30 16:16:13 +0300

    style(parsing): fix ruff rule mixed-spaces-and-tabs (E101)

--- parsing.py
+++ parsing.py
@@ -17,8 +17,7 @@ items_on_page = "&items_on_page=20"
 pages = "&page="
 # Чтобы обойти ошибку 404, добавляем заголовок. Как будто запрос делает реальный пользователь
 headers = {
-	"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-				  "Chrome/108.0.0.0 Safari/537.36"
+	"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
 }
 
 
