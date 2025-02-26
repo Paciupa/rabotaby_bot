2a20898e2321e4e7476b75a0bc293c381eafe347
Paciupa <65620103+Paciupa@users.noreply.github.com>
2025-02-26 08:19:46 +0300

    test(data): add test for Settings.get_list_codes_tables()

--- test_settings.py
+++ test_settings.py
@@ -32,3 +32,9 @@ def test_get_database_connection_parameters_without_name(expected_database_conne
 		without_database=True
 	)
 	assert actual_connection_parameters_without_database == expected_database_connection_parameters
+
+
+def test_get_list_codes_tables():
+	expected_code_tables = ["ST", "BL", "VL"]
+	actual_code_tables = Settings.get_list_codes_tables()
+	assert sorted(actual_code_tables) == sorted(expected_code_tables)
