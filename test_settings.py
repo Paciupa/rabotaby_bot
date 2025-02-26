21a2afdc56762e258513103670c61483675f293d
Paciupa <65620103+Paciupa@users.noreply.github.com>
2025-02-26 05:15:13 +0300

    refactor(data): split test for database connection parameters into three

--- test_settings.py
+++ test_settings.py
@@ -1,9 +1,13 @@
-import data
+import pytest
+
+from data import Settings
+
+# Test database connection parameters
 
 
-def test_database_connection_parameters():
-	"""Do database connection parameters correctly retrieve from environment variables?"""
-	expected_connection_parameters = {
+@pytest.fixture
+def expected_database_connection_parameters():
+	return {
 		"host": "test_localhost",
 		"port": "5439",
 		"database": "test_bot_db",
@@ -11,14 +15,20 @@ def test_database_connection_parameters():
 		"password": "p7As5s",
 	}
 
-	actual_database_name = data.Settings.get_name_database()
-	assert actual_database_name == expected_connection_parameters["database"]
 
-	actual_connection_parameters = data.Settings.get_db_connection_parameters()
-	assert actual_connection_parameters == expected_connection_parameters
+def test_get_database_name(expected_database_connection_parameters):
+	actual_database_name = Settings.get_name_database()
+	assert actual_database_name == expected_database_connection_parameters["database"]
 
-	del expected_connection_parameters["database"]
-	actual_connection_parameters_without_database = data.Settings.get_db_connection_parameters(
+
+def test_get_database_connection_parameters_full(expected_database_connection_parameters):
+	actual_connection_parameters = Settings.get_db_connection_parameters()
+	assert actual_connection_parameters == expected_database_connection_parameters
+
+
+def test_get_database_connection_parameters_without_name(expected_database_connection_parameters):
+	del expected_database_connection_parameters["database"]
+	actual_connection_parameters_without_database = Settings.get_db_connection_parameters(
 		without_database=True
 	)
-	assert actual_connection_parameters_without_database == expected_connection_parameters
+	assert actual_connection_parameters_without_database == expected_database_connection_parameters
